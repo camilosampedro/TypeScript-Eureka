3a644261bddac44e8e140b521b39e212e64a0944
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2016-11-09 15:45:18 -0500

    Added systemjs support to Index.html

---
+++ bootstrap.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by camilo on 9/11/16.
+ */
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {PonyRacerAppComponent} from './ponyracer-app.component';
+
+bootstrap(PonyRacerAppComponent)
+    .catch(err => console.log(err));
---
+++ ponyracer-app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by camilo on 9/11/16.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'ponyracer-app',
+    template: `<h1>PonyRacer</h1>`
+})
+export class PonyRacerAppComponent{
+
+}
